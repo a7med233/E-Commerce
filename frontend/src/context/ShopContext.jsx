b9e0f21dca4823b0e48818e9648e330e553aaf90
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-12-25 18:57:40 +0100

    Connecting the Cart with the DB

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -42,6 +42,17 @@ const ShopContextProvider = (props) => {
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/add', { itemId, size }, { headers: { token } })
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+        }
     }
 
     const getCartCount = () => {
@@ -68,6 +79,19 @@ const ShopContextProvider = (props) => {
         cartData[itemId][size] = quantity;
 
         setCartItems(cartData);
+
+        if (token) {
+            try {
+
+                await axios.post(backendUrl + '/api/cart/update', { itemId, size, quantity }, { headers: { token } })
+
+
+            } catch (error) {
+                console.log(error);
+                toast.error(error.message)
+            }
+
+        }
     }
 
     const getCartAmount = () => {
@@ -102,15 +126,30 @@ const ShopContextProvider = (props) => {
         }
     }
 
+    const getUserCart = async (token) => {
+        try {
+
+            const response = await axios.post(backendUrl + '/api/cart/get', {}, { headers: { token } })
+            if (response.data.success) {
+                setCartItems(response.data.cartData)
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     useEffect(() => {
         getProductsData()
     }, [])
 
-    useEffect(()=>{
+    useEffect(() => {
         if (!token && localStorage.getItem('token')) {
             setToken(localStorage.getItem('token'))
+            getUserCart(localStorage.getItem('token'))
         }
-    },[])
+    }, [])
 
     const value = {
         products, currency, delivery_fee,
