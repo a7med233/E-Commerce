25806833f73b4797de9b9a40369a2d1d5d41c243
Ahmed Emad Mohammed Abdelaziz Samra <ahmedemad23@mailbox.unideb.hu>
2024-12-25 17:56:39 +0100

    Adding the Logic for Login Page

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -7,19 +7,20 @@ export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
 
-    {/*Change the Currency symbol*/}
+    {/*Change the Currency symbol*/ }
     const currency = '$';
     const delivery_fee = 10;
     const backendUrl = import.meta.env.VITE_BACKEND_URL
-    const [search,setSearch] = useState('');
-    const [showSearch,setShowSearch] = useState(false)
-    const [cartItems,setCartItems] = useState({});
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false)
+    const [cartItems, setCartItems] = useState({});
     const [products, setProducts] = useState([])
+    const [token, setToken] = useState('')
     const navigate = useNavigate();
 
 
-    {/* Change the size Parametar */}
-    const addToCart = async (itemId,size) =>{
+    {/* Change the size Parametar */ }
+    const addToCart = async (itemId, size) => {
 
         if (!size) {
             toast.error('Select Product Color')
@@ -32,35 +33,35 @@ const ShopContextProvider = (props) => {
             if (cartData[itemId][size]) {
                 cartData[itemId][size] += 1;
             }
-            else{
+            else {
                 cartData[itemId][size] = 1;
             }
         }
-        else{
+        else {
             cartData[itemId] = {};
             cartData[itemId][size] = 1;
         }
         setCartItems(cartData);
     }
 
-    const getCartCount = () =>{
+    const getCartCount = () => {
         let totalCount = 0;
-        for(const items in cartItems){
-            for(const item in cartItems[items]){
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
                 try {
                     if (cartItems[items][item] > 0) {
                         totalCount += cartItems[items][item];
                     }
-                    
+
                 } catch (error) {
-                    
+
                 }
             }
         }
         return totalCount;
     }
 
-    const updateQuantity = async (itemId,size,quantity) => {
+    const updateQuantity = async (itemId, size, quantity) => {
 
         let cartData = structuredClone(cartItems);
 
@@ -69,17 +70,17 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData);
     }
 
-    const getCartAmount = () =>{
+    const getCartAmount = () => {
         let totalAmount = 0;
-        for(const items in cartItems){
+        for (const items in cartItems) {
             let itemInfo = products.find((products) => products._id === items);
-            for(const item in cartItems[items]){
+            for (const item in cartItems[items]) {
                 try {
                     if (cartItems[items][item] > 0) {
                         totalAmount += itemInfo.price * cartItems[items][item];
                     }
                 } catch (error) {
-                    
+
                 }
             }
         }
@@ -90,28 +91,34 @@ const ShopContextProvider = (props) => {
         try {
             const response = await axios.get(backendUrl + '/api/product/list')
             if (response.data.success) {
-                setProducts(response.data.products)                
+                setProducts(response.data.products)
             } else {
                 toast.error(response.data.message)
             }
-            
+
         } catch (error) {
             console.log(error);
             toast.error(error.message)
         }
     }
 
-    useEffect(() =>{
+    useEffect(() => {
         getProductsData()
+    }, [])
+
+    useEffect(()=>{
+        if (!token && localStorage.getItem('token')) {
+            setToken(localStorage.getItem('token'))
+        }
     },[])
 
     const value = {
-        products, currency, delivery_fee, 
+        products, currency, delivery_fee,
         search, setSearch, showSearch, setShowSearch,
-        cartItems,addToCart,
+        cartItems, addToCart,
         getCartCount, updateQuantity,
-        getCartAmount, navigate ,
-        backendUrl
+        getCartAmount, navigate,
+        backendUrl, setToken, token
     }
 
     return (
